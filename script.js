156b6e1c752bb31dc549c5cb7b09979456606dc2
anakha <anakharajendran2000@gmail.com>
2021-10-20 00:10:06 -0400

    calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+// function displayValue(num){
+
+//     let res=document.querySelector("#result")  // id koduthath ethano ath venam kodukan
+//     res.value+=num
+// }
+var displayValue=(num)=>result.value+=num
+
+var clearBox=()=>result.value=""
+
+var evaluateExpression=()=>result.value=eval(result.value)
+
+// function clearBox(){
+//     result.value=""
+// }
+
+// function evaluateExpression(){
+//     let expr=result.value;
+//     let out=eval(expr)
+//     result.value=out;
+// }
